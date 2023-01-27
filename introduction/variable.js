aff77472501ead46f003f94be87f219aa5d9f248
monoronjon sarker <monoronjons86@gmail.com>
2023-01-27 17:45:20 +0600

    variable added

---
+++ introduction/variable.js
@@ -0,0 +1,18 @@
+var number=23;
+// vary+able=variable
+
+//numeric
+var price=11;
+var age=17;
+var temp=34;
+
+//string
+var person= "monoronjon sarker";
+var location="unkown world";
+var speacal="alia bhatt"
+
+//boolean
+var serious=true;
+var isFullMarks=true;
+var isperson=false;
+
