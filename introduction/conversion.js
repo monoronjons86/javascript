270a32663985601e926a8449ad867a41ab493887
monoronjon sarker <monoronjons86@gmail.com>
2023-01-27 23:08:16 +0600

    introduction finishe

---
+++ introduction/conversion.js
@@ -0,0 +1,11 @@
+// var price1='31';
+// var price2=10;
+// var totalPrice=price1+price2;
+// console.log(totalPrice);
+
+var mom=1000;
+var apple=200;
+var oranges=50;
+var total=apple+oranges
+console.log(mom-total);
+
---
+++ introduction/operation.js
@@ -0,0 +1,4 @@
+var onionPrice=50;
+var eggPrice=10;
+console.log(onionPrice);
+console.log(onionPrice+eggPrice)
