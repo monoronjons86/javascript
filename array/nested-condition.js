2913f9f77bcc1ef32c27766744c883579f1d0cbc
monoronjon sarker <monoronjons86@gmail.com>
2023-01-28 18:19:23 +0600

    nested added

---
+++ array/nested-condition.js
@@ -0,0 +1,21 @@
+var math=true;
+var geometry=true;
+var straightLine=false;
+
+if(math==true){
+    if(geometry==true){
+        if(straightLine==true){
+
+        }
+        else{
+            console.log('baka poth a cholbo na ')
+        }
+
+    }
+    else{
+        console.log('pithagorach hoite parbo na');
+    }
+}
+else{
+
+}
