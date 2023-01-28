8168dea6d4be7818819004efbb860b64a4bc97d8
monoronjon sarker <monoronjons86@gmail.com>
2023-01-28 12:56:53 +0600

    condition added

---
+++ array/conditional.js
@@ -0,0 +1,28 @@
+// var iphonePrice=79000;
+// var myBudget=95000;
+// //if iphone price in less than my budget.I will buy the iphone
+// if(iphonePrice<myBudget)
+// {
+// console.log('i phone kine futanai marbo');
+// }
+
+
+
+var ChickenPrice=500;
+var myMoney=50;
+// if(ChickenPrice<=myMoney){
+//     console.log('murgir ran khamu and hardi chibamu');
+// }
+// if(ChickenPrice>myMoney)
+// {
+// console.log('Smashed potatc and lentils soups')
+// }
+
+if(ChickenPrice<myMoney)
+{
+    console.log('I will eat chiacken')
+}
+else
+{
+console.log('I will never eat chiackern')
+}
