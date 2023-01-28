589df568ef73bd1ce2ca48910da915e7c78acd64
monoronjon sarker <monoronjons86@gmail.com>
2023-01-28 18:46:14 +0600

    revison finished

---
+++ loop/revision.js
@@ -0,0 +1,9 @@
+var battle='water';
+var bottleColor='yellow';
+var sunglassPrize=120;
+
+var tableItem=['bottle','pen','mouse','sunglass','paper']
+var item3=tableItems[3];
+tableItem[3]='notebook'
+var penIndex=tableItem.indexOf('pen');
+
