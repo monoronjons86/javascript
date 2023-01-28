35578695f687f03e76d876c789ea5310be744f69
monoronjon sarker <monoronjons86@gmail.com>
2023-01-28 23:58:34 +0600

    for add

---
+++ loop/for.js
@@ -0,0 +1,10 @@
+//for loop
+for(var roastGiven=0;roastGiven<7;roastGiven++){
+    console.log('Roast dan,please!!');
+}
+
+//simple version write for loop
+console.log('simple for loop')
+for(var i=0;i<7;i++){
+    console.log(i);
+}
