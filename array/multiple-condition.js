8f7c1379d354cc80e517b22012672518b33c63a2
monoronjon sarker <monoronjons86@gmail.com>
2023-01-28 15:50:51 +0600

    multiple-condition

---
+++ array/multiple-condition.js
@@ -0,0 +1,26 @@
+var IsGraduated=true;
+var salary=25000;
+var cars=1;
+
+// if(IsGraduated==true){
+//     console.log('eso biya kore felai')
+// }
+// else{
+//     console.log('tor kopal a biya nai');
+// }
+
+// if(IsGraduated==true && salary>50000){
+//     console.log('eso biya kore felai')
+// }
+// else{
+//     console.log('tor kopal a biya nai');
+// }
+
+if(IsGraduated==true && salary>50000 && cars>=1 ){
+    console.log('eso biya kore felai')
+}
+else{
+    console.log('tor kopal a biya nai');
+}
+
+
