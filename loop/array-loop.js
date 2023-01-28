47d5022cd2c2b73d8129f3d07fbcbf205de637ad
monoronjon sarker <monoronjons86@gmail.com>
2023-01-29 00:30:33 +0600

    added

---
+++ loop/array-loop.js
@@ -0,0 +1,17 @@
+//again for loop practice
+
+// for(var i=0;i<10;i++)
+// {
+//     console.log(i);
+// }
+
+//target:display every elements of an array
+
+var numbers=[47,45,49,44,34,88,89,77];
+for(var i=0;i<numbers.length;i++)
+{
+    var number=numbers[i];
+    console.log(number);
+
+}
+
