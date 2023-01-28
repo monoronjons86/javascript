60dd6fbca9c96848c99498751620802bcb7f26ed
monoronjon sarker <monoronjons86@gmail.com>
2023-01-28 16:43:17 +0600

    multi-stage-condition

---
+++ array/multi-stage-condition.js
@@ -0,0 +1,16 @@
+var money=30;
+var danishPrice=45;
+var butterBread=35;
+var toastBiscuit=20;
+if(danishPrice<money){
+    console.log('Dan dan danish khabo');
+}
+else if(butterBread<money){
+    console.log('butter bon khamu');
+}
+else if(toastBiscuit<money){
+    console.log('tost bisucuit khamu');
+}
+else{
+    console.log('khali cha e sasthor jrne vlo')
+}
