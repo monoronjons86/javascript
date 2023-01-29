34fe66132538abff7111ba3add8bc4cb966e515b
monoronjon sarker <monoronjons86@gmail.com>
2023-01-29 09:42:06 +0600

    apple-orang

---
+++ loop/apple-orang.js
@@ -0,0 +1,35 @@
+// var manyGiven=1000;
+// var applePrice=400;
+// var orangPrice=300;
+
+// var totalPrice=applePrice+orangPrice;
+// console.log(totalPrice);
+// var giveBack=manyGiven-totalPrice;
+// console.log(giveBack);
+
+//75.25,65,80,35.45,99.50
+//Mathematic,Biology,Chemistry,Physics and Bangla
+
+var mathMarks=75.25;
+var biologyMarks=65;
+var chemistryMarks=80;
+var physicsMarks=35.45;
+var BanglaMarks=50;
+
+var totalMarks=mathMarks+biologyMarks+chemistryMarks+physicsMarks+BanglaMarks;
+console.log(totalMarks)
+var average=totalMarks / 5;
+
+var averageTwoDecimal=average.toFixed(2);
+console.log(averageTwoDecimal);
+
+
+var number=119;
+var reminder=number%5;
+console.log(reminder);
+
+var nam1="I am going to be"
+var nam2="an awesome web web developer"
+
+var total=nam1+" "+nam2;
+console.log(total);
