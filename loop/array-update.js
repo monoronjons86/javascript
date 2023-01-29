311e992a960a45154b957250876186ee5b1c1beb
monoronjon sarker <monoronjons86@gmail.com>
2023-01-29 16:17:03 +0600

    large added

---
+++ loop/array-update.js
@@ -0,0 +1,8 @@
+var fruits=['Apple','Banana','Orang']
+var bananaIndex=fruits.indexOf('Banana')
+fruits[1]='mango';
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push('watermelon')
+console.log(fruits);
---
+++ loop/large.js
@@ -0,0 +1,20 @@
+var nam1=83;
+var nam2=79;
+var nam3=145;
+
+if(nam1>nam2){
+    if(nam1>nam3){
+        console.log(nam1);
+    }
+    else{
+        console.log(nam3)
+    }
+}
+else{
+    if(nam2>nam3){
+        console.log(nam2);
+    }
+    else{
+        console.log(nam3);
+    }
+}
