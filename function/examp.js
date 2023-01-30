63f57c1b11cc81990874d8135cf022b1a0041c18
monoronjon sarker <monoronjons86@gmail.com>
2023-01-30 23:23:07 +0600

    switch.js

---
+++ function/examp.js
@@ -0,0 +1,23 @@
+//array vs object
+var shoppingItems=['books','sunglass','shoes','pen'];
+var friendsAge=[12,45,78,12,32,14]
+var friendsAge={
+    rahim:12,
+    samad:45,
+    karim:78,
+    jamal:12,
+    robi:32,
+    kazi:14
+}
+
+var shoppingCart={
+    book:3,
+    sunglass:1,
+    keyboard:5,
+    mouse:1,
+    pen:5,
+    shoes:2
+}
+
+const keys=object.keys(shoppingCart)
+console.log(keys)
