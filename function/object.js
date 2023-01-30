61b111fe1d6fa340c58c2f690ee368e373596754
monoronjon sarker <monoronjons86@gmail.com>
2023-01-30 16:58:50 +0600

    object add

---
+++ function/object.js
@@ -0,0 +1,19 @@
+var student={
+    id:115,
+    name:'solamin khan',
+    class:9,
+    marks:98
+}
+var mobile={
+    brand:'samsung',
+    price:1900,
+    Storage:'64gb',
+    camera:'7mp',
+}
+var Mycomputer={
+    brand:'lenovo',
+    price:3900,
+    color:'silver',
+    processor:'i7'
+}
+console.log(Mycomputer.price)
