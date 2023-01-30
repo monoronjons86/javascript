b91e4693384e73b962fb5c41a1091318726ef782
monoronjon sarker <monoronjons86@gmail.com>
2023-01-30 10:45:41 +0600

    parametar finished

--- function/parametar.js
+++ function/parametar.js
@@ -1,5 +1,25 @@
+// function bringSingara(money){
+//     console.log('mama singara dan');
+//     console.log(money);
+// }
+// bringSingara(100);
+
 function bringSingara(money){
-    console.log('mama singara dan');
-    console.log(money);
+    console.log('ato tk diyacen',money);
+    console.log('ai nan singara');
 }
 bringSingara(100);
+
+
+function add(nam1,nam2){
+    console.log('going to add:',nam1,nam2)
+
+}
+add(125,136);
+
+function sum(a,b,c,d,e,f){
+    var sum=a+b+c+d+e+f;
+    console.log(sum);
+
+}
+sum(98,88,44,57);
