604ea2442e98b2cdaa1dd6b9d05b133aac24c535
monoronjon sarker <monoronjons86@gmail.com>
2023-01-31 19:09:42 +0600

    inchToFeet added

---
+++ apply/inchToFeet.js
@@ -0,0 +1,28 @@
+// const myInches=12;
+// const myFeet=myInches/12;
+// console.log(myFeet);
+
+// const dataInches=144;
+// const dadafeet=dataInches/12;
+// console.log(dadafeet);
+
+// const dadiInches=60;
+// const dadifeet=dadiInches/12;
+// console.log(dadifeet);
+
+// function inchToFeet(inches){
+//     const feet=inches/12;
+//     return feet;
+
+// }
+// const dadaInches=144;
+// const dadaFeet=inchToFeet(dadaInches);
+// console.log(dadaFeet);
+
+function inchToFeet(inches){
+    const feet=inches/12;
+    return feet;
+}
+const dadaInches=144;
+const dadaFeet=inchToFeet(dadaInches);
+console.log(dadaFeet); 
--- apply/let.js
+++ apply/let.js
@@ -2,3 +2,6 @@
 var friends=['aboul','tablul','kobul','mokbul'];
 const myfrinds=friends[1];
 console.log(myfrinds);
+
+let country='Bangladesh'
+console.log(country)
