11cb5d1cf3cccf90fd5e76f4095f687dee0c9ae9
monoronjon sarker <monoronjons86@gmail.com>
2023-01-31 12:37:12 +0600

    multi-condition

--- array/multiple-condition.js
+++ array/multiple-condition.js
@@ -22,5 +22,20 @@ if(IsGraduated==true && salary>50000 && cars>=1 ){
 else{
     console.log('tor kopal a biya nai');
 }
+var nam1=50;
+var nam2=30;
+var nam3=20;
+if(nam1>nam2 && nam2>nam1){
+    console.log('it is true')
+}
+else{
+    console.log('it is false')
+}
+if(nam2<nam1 || nam1>nam2){
+    console.log('this is well')
+}
+else{
+    console.log('this is not well')
+}
 
 
