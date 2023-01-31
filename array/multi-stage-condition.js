555c0df25691ebf30e050fa7634880fb40ab5885
monoronjon sarker <monoronjons86@gmail.com>
2023-01-31 11:40:57 +0600

    multi practice

--- array/multi-stage-condition.js
+++ array/multi-stage-condition.js
@@ -14,3 +14,19 @@ else if(toastBiscuit<money){
 else{
     console.log('khali cha e sasthor jrne vlo')
 }
+var money1=20;
+var tomattoPrice=45;
+var eatPrice=40;
+var potatoPrice=30
+if(tomattoPrice<money1){
+    console.log(' tomatto khamu');
+}
+else if(eatPrice<money1){
+    console.log('amit eat khamu');
+}
+else if(potatoPrice<money1){
+    console.log('potato khamu');
+}
+else{
+    console.log('khali jhalmuri khabo');
+}
