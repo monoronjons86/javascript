eee8314bcd750c31aaf1f5baeb60153c208b8c5f
monoronjon sarker <monoronjons86@gmail.com>
2023-01-31 18:04:09 +0600

    let added

---
+++ apply/let.js
@@ -0,0 +1,4 @@
+// array
+var friends=['aboul','tablul','kobul','mokbul'];
+const myfrinds=friends[1];
+console.log(myfrinds);
--- array/practice.js
+++ array/practice.js
@@ -1,5 +1,17 @@
-var numbers=[20,30,40,50,65]
-numbers.push(70);
-numbers[1]=44;
-numbers.pop();
-console.log(numbers);
+// var numbers=[20,30,40,50,65]
+// numbers.push(70);
+// numbers[1]=44;
+// numbers.pop();
+// console.log(numbers);
+
+var math=true;
+var geometry=true;
+var straightLine=false;
+
+if (math==true){
+
+
+}
+else{
+    
+}
