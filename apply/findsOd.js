3c1ae55fc9697ff2bd821c9dd389ccb7f5a49be9
monoronjon sarker <monoronjons86@gmail.com>
2023-02-01 20:38:13 +0600

    finished

--- apply/findsOd.js
+++ apply/findsOd.js
@@ -4,10 +4,27 @@ function getSum(numbers){
         const index=i;
         const element=numbers[index]
         sum=sum+element;
-        console.log(index,element,sum);
+        // console.log(index,element,sum);
     }
       
 }
 
-const myNumber=[20,30,40,50,70,80]
+function getOddNumbersofAnArray(numbers){
+    const ooddNumbers=[];
+    for(let i=0;i<numbers.length; i++){
+        const index=i;
+        const element=numbers[index];
+        if(element%2!==0){
+            console.log(index,element);
+            ooddNumbers.push(element);
+        }
+       
+    }
+    return ooddNumbers;
+
+}
+
+const myNumber=[20,30,40,50,70,80,85,75];
+ const oddNumbers=getOddNumbersofAnArray(myNumber);
+ console.log(oddNumbers);
 getSum(myNumber);
