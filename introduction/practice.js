8915501c712eccc4e01507597f1f3bebb96e7ea0
monoronjon sarker <monoronjons86@gmail.com>
2023-02-03 09:22:55 +0600

    added

--- introduction/practice.js
+++ introduction/practice.js
@@ -1,6 +1,18 @@
-var person='monoronjon sarker';
-var location='Rangpur';
+// var person='monoronjon sarker';
+// var location='Rangpur';
 
-var serious=true;
-are you single=false;
+// var serious=true;
+// are you single=false;
+
+var price1='20';
+var price2=30;
+var total=price1+price2;
+console.log(total); 
+var priceMone=parseInt(price1);
+console.log(priceMone);
+
+var gpa="3.40";
+var average=parseFloat(gpa);
+console.log(average);
+
 
