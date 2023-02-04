69ef6795a38e424d440461504d6c4a332522d9e7
monoronjon sarker <monoronjons86@gmail.com>
2023-02-04 11:43:34 +0600

    for added

--- loop/practice.js
+++ loop/practice.js
@@ -14,9 +14,26 @@
 
 // 1 to 10 odd numbers
 
-var number=1;
-while(number<=20){
-    console.log(number);
-    number=number+2
+// var number=1;
+// while(number<=20){
+//     console.log(number);
+//     number=number+2
 
+// }
+
+var rostGiven=0;
+while(rostGiven<10){
+    console.log(rostGiven);
+    rostGiven++;
+}
+
+// again practice try for loop
+
+for(var rostGiven=0;rostGiven<20;rostGiven++)
+{
+    console.log('is for loop',rostGiven);
+}
+//i=i+2
+for(var i=1;i<=10;i+=2){
+    console.log(i);
 }
