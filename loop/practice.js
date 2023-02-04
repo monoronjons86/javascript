47d5bf1461f1bc2e65d74e49f5f4d98f3a7acda4
monoronjon sarker <monoronjons86@gmail.com>
2023-02-04 10:32:32 +0600

    while added

---
+++ loop/practice.js
@@ -0,0 +1,12 @@
+// var rostGive=0;
+// while(rostGive<7){
+//     console.log('rost dan pls');
+//     rostGive++;
+
+// }
+
+numbers=1;
+while(numbers<=10){
+    console.log(numbers);
+    numbers++;
+}
