b726f2cb8ac080090fbc1337f3a7911bd1c76f29
monoronjon sarker <monoronjons86@gmail.com>
2023-02-04 13:30:49 +0600

    finished reverse

--- loop/practice.js
+++ loop/practice.js
@@ -38,8 +38,45 @@ while(rostGiven<10){
 //     console.log(i);
 // }
 
-var numbers=[25,30,35,40,45,50,55,60,65,70]
-for(var i=0;i<numbers.length; i++){
-    var number=numbers[i];
-    console.log(number);
+// var numbers=[25,30,35,40,45,50,55,60,65,70]
+// for(var i=0;i<numbers.length; i++){
+//     var number=numbers[i];
+//     console.log(number);
+// }
+
+// for(var i=1;i<20;i++){
+//     console.log(i);
+//     if(i>12){
+//         break;
+//     }
+
+// }
+
+// var rostGiven=0;
+// while(rostGiven<10){
+//     console.log('rost dan gift anci')
+//     rostGiven++;
+//     if(rostGiven>4){
+//         break;
+//     }
+// }
+
+// var items=['bottle','mouse','sunglass','pen','notebook']
+// for(var i=0;i<items.length;i++){
+//     var item=items[i]
+//     if(item=='mouse'){
+//         break;
+//     }
+//     console.log(item);
+    
+// }
+
+var num=10;
+while(num>=1){
+    console.log(num);
+    num--;
+}
+
+for(var i=10;i>=1;i--){
+    console.log(i);
 }
