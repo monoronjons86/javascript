f460c73ba95296af0635f09057682b9edd1d6a60
monoronjon sarker <monoronjons86@gmail.com>
2023-02-05 09:28:57 +0600

    aj amr mon vlo nai

---
+++ workPractice.js
@@ -0,0 +1,29 @@
+// var MyFriend=['abul','kobul','babul','dabul','korim']
+// MyFriend[3]='topul';
+// console.log(MyFriend);
+
+// var mytk=90000
+// var mac=800000;
+// var gamLaptop=6000;
+// var lenvo=4000;
+// var laptop=2000;
+// if(mytk>mac){
+//     console.log('mac kinmu')
+// }
+// else if(mytk<gamLaptop){
+// console.log('gamlaptop kinmu')
+// }
+// else if(mytk<lenvo){
+//     console.log('lenvo kinbo')
+// }
+// else if(mytk<laptop){
+//     console.log('laptop kinbo')
+// }
+// else{
+//     console.log('mobile a code kore chalamu')
+// }
+
+
+for(var i=1;i<=39;i++){
+    console.log('aj amar mon vlo nai')
+}
