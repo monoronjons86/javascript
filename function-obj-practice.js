421310d7adc14477442fa8998dafe191e1076af2
monoronjon sarker <monoronjons86@gmail.com>
2023-02-05 18:04:14 +0600

    milles to killo practice

--- function-obj-practice.js
+++ function-obj-practice.js
@@ -5,20 +5,61 @@
 // }
 // sum(20,30,40,50,55,70,75)
 
-function add(number1,number2){
-    console.log(number1,number2);
-    var sum=number1+number2
-    return sum;
+// function add(number1,number2){
+//     console.log(number1,number2);
+//     var sum=number1+number2
+//     return sum;
+
+// }
+// var total=add(20,30)
+// console.log('total',total);
+
+// function bringSingra(money){
+//     var singraPrice=10;
+//     var total=money / singraPrice;
+//     return total;
+// }
+// var myTk=200;
+//  var avg=bringSingra(myTk);
+//  console.log('price',avg);
+
+// var shopingCart={
+//     book:3,
+//     sunglass:2,
+//     keybord:5,
+//     mouse:4,
+//     pen:24,
+//     shoes:2,
+// }
+// console.log(shopingCart.book)
+
+// const MyInches=12;
+// const MyFeet=MyInches/12;
+// console.log(MyFeet)
+
+// const dadaInches=144;
+// const dadaFeat=dadaInches/12;
+// console.log('dadaFeat',dadaFeat);
+
+// const dadaiInches=60;
+// const dadiFeat=dadaiInches/12;
+// console.log('dadiFeat',dadiFeat);
+
+function inchToFeat(inches){
+    const feet=inches/12;
+    return feet;
 
 }
-var total=add(20,30)
-console.log('total',total);
+const dadaInches=144;
+const dadaFeat=inchToFeat(dadaInches);
+console.log(dadaFeat)
 
-function bringSingra(money){
-    var singraPrice=10;
-    var total=money / singraPrice;
-    return total;
+
+function milesTokillometar(miles){
+    const killometar=miles*1.609;
+    return killometar;
 }
-var myTk=200;
- var avg=bringSingra(myTk);
- console.log('price',avg);
+
+var killo=2.4;
+var tortal=milesTokillometar(killo);
+console.log('tortal',tortal)
