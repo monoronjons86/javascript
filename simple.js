20926b4bc52ae3e7d63cd3e50d2a5e4d9f132f6b
monoronjon sarker <monoronjons86@gmail.com>
2023-02-07 22:53:27 +0600

    reverse

--- simple.js
+++ simple.js
@@ -1,5 +1,20 @@
-let sum=0;
-for(let i=1;i<=5;i++){
-    sum=sum+i;
+// let sum=0;
+// for(let i=1;i<=5;i++){
+//     sum=sum+i;
+// }
+// console.log(sum);
+
+// let sum=0;
+// for(let i=5;i>=1;i--){
+//     sum=sum+i
+// }
+// console.log(sum);
+
+function sum(i){
+    if(i==1){
+        return 1;
+    }
+    return i +sum(i-1);
 }
-console.log(sum);
+const result=sum(5);
+console.log(result);
