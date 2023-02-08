a7de9798d06b29c0eba9ec70453a4bba4794d2a4
monoronjon sarker <monoronjons86@gmail.com>
2023-02-08 21:11:14 +0600

    added

---
+++ object.js
@@ -0,0 +1,13 @@
+const student={
+    name:"monoronjon sarker",
+    id:121,
+    address:'movie cinema',
+    isSingle:true,
+    friends:['Apu','Raaz','salman','Amair'],
+    car:{
+        brand:'tesla',
+        price:50000,
+        made:2020,
+    }
+}
+console.log(student);
