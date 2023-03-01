f799882895a881224d158fa42fb84a5881d944df
monoronjon sarker <monoronjons86@gmail.com>
2023-03-01 22:51:59 +0600

    addd

--- pin-matcher/js/app.js
+++ pin-matcher/js/app.js
@@ -6,18 +6,12 @@ function getPin(){
         return pin;
     }
     else{
-        console.log('pin not 3 digit found',pin)
-        return getPin()
+        console.log('pin not 3 digit',pin)
+        return getPin();
     }
 }
 function generatePin(){
-    const random=math.round(math.random()*10000);
+    const random=Math.round(Math.random()*10000);
     return random;
 }
-document.getElementById('generate-pin').addEventListener('click',function(){
-    const pin=getPin();
-    //display pin
-    const displayPinField=document.getElementById('display-pin');
-    displayPinField.value =pin;
-   
-});
+  
