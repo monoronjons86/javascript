ff4d8459919eb36a919cb38671708ad211dc7e47
monoronjon sarker <monoronjons86@gmail.com>
2023-03-02 12:30:20 +0600

    mm

--- pin-matcher/js/app.js
+++ pin-matcher/js/app.js
@@ -14,4 +14,25 @@ function generatePin(){
     const random=Math.round(Math.random()*10000);
     return random;
 }
+document.getElementById('generate-pin').addEventListener('click',function(){
+    const pin=getPin();
+    const displayPinField=document.getElementById('display-pin');
+    displayPinField.value =pin;
+});
+
+document.getElementById('calculator').addEventListener('click',function(event){
+    const Number=event.target.innerText;
+    const typedNumberField=document.getElementById('typed-numbers');
+    if(isNaN(Number)){
+        if(Number==='c'){
+            typedNumberField.value=' ';
+        }
+    }
+    else{
+        const previousTypedNumber=typedNumberField.value;
+        const newTypedNumber=previousTypedNumber + Number;
+        typedNumberField.value=newTypedNumber;
+    }
+
+});
   
