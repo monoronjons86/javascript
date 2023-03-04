2c9d1936d88636ae424dd61a8b85d5ed0fe1c6fc
monoronjon sarker <monoronjons86@gmail.com>
2023-03-04 15:45:11 +0600

    quantity

---
+++ shopping-card-using-dom/index.js
@@ -0,0 +1,22 @@
+document.getElementById('first-card').addEventListener('click',function(){
+    //get the dat7a
+    const productName=document.getElementById('first-name').innerText;
+    const productPrice=document.getElementById('first-price').innerText;
+    const productQuantity=document.getElementById('first-quantity').innerText;
+    console.log(productName,productPrice,productQuantity);
+    const container=document.getElementById('table-container')
+
+    const tr=document.createElement("tr");
+    tr.innerHTML=`
+     <td>${1}</td>
+     <td>${productName}</td>
+     <td>${productPrice}</td>
+     <td>${productQuantity}</td>
+
+
+    
+    `;
+    container.appendChild(tr);
+    
+   
+});
