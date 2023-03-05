9a71e917d1d228331c9b086e77c79d549da4a664
monoronjon sarker <monoronjons86@gmail.com>
2023-03-05 08:09:38 +0600

    first-card

--- shopping-card-using-dom/index.js
+++ shopping-card-using-dom/index.js
@@ -3,17 +3,29 @@ document.getElementById('first-card').addEventListener('click',function(){
     const productName=document.getElementById('first-name').innerText;
     const productPrice=document.getElementById('first-price').innerText;
     const productQuantity=document.getElementById('first-quantity').innerText;
-    console.log(typeof productName, typeof productPrice,typeof   productQuantity);
+    console.log( productName, productPrice, productQuantity);
 
     const priceTotal=parseInt(productPrice) *parseInt(productQuantity) ;
     console.log(priceTotal);
-
-
-
-    const container=document.getElementById('table-container')
-
+    
 
      //show the data 
+     displayData(productName,productPrice,productQuantity,priceTotal);
+
+    
+   
+});
+//using even object form browser
+document.getElementById('second-card').addEventListener('click',function(e){
+  console.log(e.target.);
+
+});
+
+
+//common function to display data
+function displayData(productName,productPrice,productQuantity,priceTotal){
+    const container=document.getElementById('table-container')
+
     const tr=document.createElement("tr");
     tr.innerHTML=`
      <td>${1}</td>
@@ -21,11 +33,6 @@ document.getElementById('first-card').addEventListener('click',function(){
      <td>${productPrice}</td>
      <td>${productQuantity}</td>
      <td>${priceTotal}</td>
-
-
-    
     `;
     container.appendChild(tr);
-    
-   
-});
+}
--- shopping-card-using-dom/test.js
+++
@@ -1,4 +0,0 @@
-const firstName="rakib";
-const lastName="hasan";
-const fullName=`${firstName} ${lastName} ${7+5}`;
-console.log(fullName);
