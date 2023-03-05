18e0cb785b98baa1f2dc3112f100807751128a79
monoronjon sarker <monoronjons86@gmail.com>
2023-03-05 06:24:04 +0600

    addd

--- shopping-card-using-dom/index.js
+++ shopping-card-using-dom/index.js
@@ -3,15 +3,24 @@ document.getElementById('first-card').addEventListener('click',function(){
     const productName=document.getElementById('first-name').innerText;
     const productPrice=document.getElementById('first-price').innerText;
     const productQuantity=document.getElementById('first-quantity').innerText;
-    console.log(productName,productPrice,productQuantity);
+    console.log(typeof productName, typeof productPrice,typeof   productQuantity);
+
+    const priceTotal=parseInt(productPrice) *parseInt(productQuantity) ;
+    console.log(priceTotal);
+
+
+
     const container=document.getElementById('table-container')
 
+
+     //show the data 
     const tr=document.createElement("tr");
     tr.innerHTML=`
      <td>${1}</td>
      <td>${productName}</td>
      <td>${productPrice}</td>
      <td>${productQuantity}</td>
+     <td>${priceTotal}</td>
 
 
     
---
+++ shopping-card-using-dom/test.js
@@ -0,0 +1,4 @@
+const firstName="rakib";
+const lastName="hasan";
+const fullName=`${firstName} ${lastName} ${7+5}`;
+console.log(fullName);
